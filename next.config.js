3a8ddd7f2ab8fd4d6c43589103756b35728f0a94
Eric Malcolm <eric@malcolmdigital.com>
2024-04-22 19:54:27 -0400

    adds configs, updates alt tags, images, cloudinary config, etc.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/my-account/**",
-      },
-    ],
-  },
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				pathname: '/my-account/**',
+			},
+		],
+		domains: ['res.cloudinary.com'],
+	},
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-        </Head>
-        <body className="bg-black antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang='en'>
+				<Head>
+					<link rel='icon' href='/favicon.ico' />
+					<meta
+						name='description'
+						content='Quick image gallery for celebrating 10 years of Eric and Erin being together.'
+					/>
+					<meta property='og:site_name' content='Eric & Erin - 10 Years!' />
+					<meta
+						property='og:description'
+						content='See pictures from Next.js Conf and the After Party.'
+					/>
+					<meta property='og:title' content='Eric & Erin - 10 Years!' />
+				</Head>
+				<body className='bg-black antialiased'>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
